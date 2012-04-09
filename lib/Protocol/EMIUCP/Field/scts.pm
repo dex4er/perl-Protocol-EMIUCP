9658c55e2317ea1e2346bdc0c6107f83dda7ea01
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-04-10 01:08:36 +0200

    new field object for scts

---
+++ lib/Protocol/EMIUCP/Field/scts.pm
@@ -0,0 +1,33 @@
+package Protocol::EMIUCP::Field::scts;
+
+use 5.008;
+
+our $VERSION = '0.01';
+
+
+use Moose;
+
+use overload (
+    q{""}    => 'as_string',
+    fallback => 1
+);
+
+use Protocol::EMIUCP::Types;
+use DateTime::Format::SCTS;
+
+has value => (is => 'ro', isa => 'SCTS', coerce => 1, required => 1);
+
+sub as_string {
+    my ($self) = @_;
+    return $self->value;
+};
+
+sub as_datetime {
+    my ($self) = @_;
+    return DateTime::Format::SCTS->parse_datetime($self->value);
+};
+
+
+__PACKAGE__->meta->make_immutable();
+
+1;
