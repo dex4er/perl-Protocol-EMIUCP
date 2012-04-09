ef5544d65bfaa2247ce0d3528aa818013cf06478
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-04-09 17:36:49 +0200

    ucpdecode

---
+++ examples/ucpdecode.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use Protocol::EMIUCP;
+use Data::Dumper ();
+
+my $str = $ARGV[0] || die "Usage: $0 ucp_string\n";
+
+my $msg = Protocol::EMIUCP->new_message_from_string($str);
+
+print Data::Dumper->new([ $msg->as_hashref ])->Quotekeys(0)->Sortkeys(1)->Terse(1)->Dump;
