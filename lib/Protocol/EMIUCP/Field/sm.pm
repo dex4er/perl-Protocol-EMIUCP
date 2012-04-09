df82e5bfe60e64efe404f520cc0ac78745e41b26
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-04-09 17:36:09 +0200

    sm object

---
+++ lib/Protocol/EMIUCP/Field/sm.pm
@@ -0,0 +1,34 @@
+package Protocol::EMIUCP::Field::sm;
+
+use 5.008;
+
+our $VERSION = '0.01';
+
+
+use Moose;
+
+use overload (
+    q{""}    => 'as_string',
+    fallback => 1
+);
+
+use Protocol::EMIUCP::Types;
+
+has adc  => (is => 'ro', isa => 'Num16', required => 1);
+has scts => (is => 'ro', isa => 'Num12', required => 1);
+
+around BUILDARGS => sub {
+    my ($orig, $class, %args) = @_;
+    if (defined $args{value} and $args{value} =~ /^(\d*):(\d*)$/) {
+        @args{qw( adc scts )} = ($1, $2);
+    };
+    return $class->$orig(%args);
+};
+
+sub as_string {
+    my ($self) = @_;
+    return sprintf '%s:%s', $self->adc, $self->scts;
+};
+
+
+1;
