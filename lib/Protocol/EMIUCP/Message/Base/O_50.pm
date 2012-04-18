056fa05c75d4505c0be6a0bf17801e3a8291c402
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-04-18 12:42:36 +0200

    Moved some logic to base class O_50

--- lib/Protocol/EMIUCP/Message/Base/O_50.pm
+++ lib/Protocol/EMIUCP/Message/Base/O_50.pm
@@ -9,8 +9,37 @@ our $VERSION = '0.01';
 
 use base qw(Protocol::EMIUCP::Message::Base::O);
 
+use Carp qw(confess);
+use Protocol::EMIUCP::Util qw( from_7bit_hex_to_utf8 from_utf8_to_7bit_hex );
+
 __PACKAGE__->make_accessors( [qw( adc oadc ac nrq nadc nt npid lrq lrad lpid dd )] );
 
+sub build_args {
+    my ($class, $args) = @_;
+
+    $args->{oadc} = from_utf8_to_7bit_hex $args->{oadc_utf8}
+        if defined $args->{oadc_utf8};
+
+    return $class;
+};
+
+sub validate {
+    my ($self) = @_;
+
+    confess "Attribute (adc) is invalid"
+        unless $self->{adc}  =~ /^\d{1,16}$/;
+    confess "Attribute (oadc) is invalid"
+        unless $self->{oadc}  =~ /^\d{1,16}|[\dA-F]{2,22}$/;
+
+    return $self->SUPER::validate;
+};
+
 use constant list_data_field_names => [ qw( adc oadc ac nrq nadc nt npid lrq lrad lpid dd ) ];
+
+sub build_hashref {
+    my ($self, $hashref) = @_;
+    $hashref->{oadc_utf8} = $self->oadc_utf8 if defined $hashref->{oadc}; # TODO and $hashref->{otoa} eq '5039'
+    return $self;
+};
 
 1;
--- lib/Protocol/EMIUCP/Message/O_51.pm
+++ lib/Protocol/EMIUCP/Message/O_51.pm
@@ -18,10 +18,8 @@ use Protocol::EMIUCP::Util qw( from_7bit_hex_to_utf8 from_utf8_to_7bit_hex );
 sub build_args {
     my ($class, $args) = @_;
 
-    $args->{oadc} = from_utf8_to_7bit_hex $args->{oadc_utf8}
-        if defined $args->{oadc_utf8};
-
-    return $class->build_ot_51_args($args);
+    return $class->SUPER::build_args($args)
+                 ->build_ot_51_args($args);
 };
 
 sub validate {
@@ -29,13 +27,8 @@ sub validate {
 
     confess "Attribute (adc) is required"
         unless defined $self->{adc};
-    confess "Attribute (adc) is invalid"
-        unless $self->{adc}  =~ /^\d{1,16}$/;
-
     confess "Attribute (oadc) is required"
         unless defined $self->{oadc};
-    confess "Attribute (oadc) is invalid"
-        unless $self->{oadc}  =~ /^\d{1,16}|[\dA-F]{2,22}$/;
 
     return $self->SUPER::validate
                 ->validate_ot_51;
@@ -48,12 +41,6 @@ use constant list_valid_lpid_codes => [ qw( 0100 0122 0131 0138 0139 0339 0439 0
 sub oadc_utf8 {
     my ($self) = @_;
     return from_7bit_hex_to_utf8 $self->{oadc};
-};
-
-sub build_hashref {
-    my ($self, $hashref) = @_;
-    $hashref->{oadc_utf8} = $self->oadc_utf8 if defined $hashref->{oadc}; # TODO and $hashref->{otoa} eq '5039'
-    return $self;
 };
 
 1;
