e0bde613fe6286e4ee740c31ac6d126a3268a3d0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-04-19 20:14:47 +0200

    vp field missing role

---
+++ lib/Protocol/EMIUCP/Message/Role/Field/vp.pm
@@ -0,0 +1,50 @@
+package Protocol::EMIUCP::Message::Role::Field::vp;
+
+use 5.006;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use Carp qw(confess);
+use Scalar::Util qw(blessed);
+use Protocol::EMIUCP::Util qw(has);
+eval { require DateTime::Format::EMIUCP::VP };
+
+has 'vp';
+
+sub build_vp_args {
+    my ($class, $args) = @_;
+
+    $args->{vp} = DateTime::Format::EMIUCP::VP->format_datetime($args->{vp})
+        if blessed $args->{vp} and $args->{vp}->isa('DateTime');
+
+    return $class;
+};
+
+sub validate_vp {
+    my ($self) = @_;
+
+    confess "Attribute (vp) is invalid"
+        if defined $self->{vp} and not $self->{vp}  =~ /^\d{10}$/;
+
+    return $self;
+};
+
+sub vp_datetime {
+    my ($self) = @_;
+
+    return unless defined $self->{vp};
+    return DateTime::Format::EMIUCP::VP->parse_datetime($self->{vp});
+};
+
+sub build_vp_hashref {
+    my ($self, $hashref) = @_;
+    if (defined $hashref->{vp} and eval { DateTime::Format::EMIUCP::VP->VERSION }) {
+        $hashref->{vp_datetime} = $self->vp_datetime->datetime;
+    };
+    return $self;
+};
+
+1;
