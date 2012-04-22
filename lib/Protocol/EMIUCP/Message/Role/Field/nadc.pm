4a17952d791d92f282b5df819a7342bb6a887115
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-04-22 23:51:51 +0200

    nadc address for 0539 have to be exaclty 16 digits

--- lib/Protocol/EMIUCP/Message/Role/Field/nadc.pm
+++ lib/Protocol/EMIUCP/Message/Role/Field/nadc.pm
@@ -29,18 +29,23 @@ sub build_args_nadc {
 };
 
 {
-    my $ip4dec  = '(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})';
-    my $tcpport = '(999[0-9]|99[0-8][0-9]|9[0-8][0-9]{2}|[0-8]?[0-9]{1,3})';
+    my $ip4dec   = '(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})';
+    my $tcpport  = '(999[0-9]|99[0-8][0-9]|9[0-8][0-9]{2}|[0-8]?[0-9]{1,3})';
 
     sub _from_addr_to_nadc {
         my ($self, $addr) = @_;
         $addr =~ /^$ip4dec\.$ip4dec\.$ip4dec\.$ip4dec:$tcpport$/ or return;
         return sprintf '%03d%03d%03d%03d%d', $1, $2, $3, $4, $5;
     };
+}
+
+{
+    my $ip4dec0  = '(25[0-5]|2[0-4][0-9]|[0-1][0-9]{1,2})';
+    my $tcpport0 = '([0-9]{4})';
 
     sub _from_nadc_to_addr {
         my ($self, $nadc) = @_;
-        $nadc =~ /^$ip4dec$ip4dec$ip4dec$ip4dec$tcpport$/ or return;
+        $nadc =~ /^$ip4dec0$ip4dec0$ip4dec0$ip4dec0$tcpport0$/ or return;
         return sprintf '%d.%d.%d.%d:%d', $1, $2, $3, $4, $5;
     };
 }
