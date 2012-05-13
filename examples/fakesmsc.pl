6a49ed564775be6f6173a340c635285192c5a1f1
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-05-14 01:08:51 +0200

    fakesmsc handles O/01, O/51 and O/60 only and returns an error otherwise

--- examples/fakesmsc.pl
+++ examples/fakesmsc.pl
@@ -1,38 +1,74 @@
 #!/usr/bin/perl
 
+# The simplest fake SMSC
+#
+# Example:
+#     fakesmsc.pl Listen=1 LocalAddr=localhost:5000
+
 use strict;
 use warnings;
 
 use lib 'lib', '../lib';
 
-use Protocol::EMIUCP;
+use Protocol::EMIUCP::Message;
 
-use IO::Handle;
+use IO::Socket::INET;
 use IO::Select;
 
-my $fhin = *STDIN{IO};
-my $selin = IO::Select->new($fhin);
+use Scalar::Util qw(blessed);
 
-my $fhout = *STDOUT{IO};
-my $selout = IO::Select->new($fhout);
+die "Usage: $0 Opt=value Opt=value\n\tSee IO::Socket::INET for accepted options\n" unless @ARGV;
 
-my $rbuf = my $wbuf = my $eof = '';
+my %opts = map { /^(.*?)=(.*)$/ and ($1 => $2) } @ARGV;
 
-SELECT: while (not $eof) {
-    while (length $wbuf and my @ready = $selout->can_write(0)) {
-        my $len = syswrite $fhout, $wbuf, 1024;
-        $wbuf = substr $wbuf, $len if $len;
-    };
-    if (my @ready = $selin->can_read(0)) {
-        my $len = sysread $fhin, my $tmp, 1024;
-        $rbuf .= $tmp if $len;
-        while ($rbuf =~ s/.*?\x02(.*?)\x03//s) {
-            my $msg = Protocol::EMIUCP::Message->new_from_string($1);
-            if ($msg->o_r eq 'O') {
-                my $rpl = Protocol::EMIUCP::Message->new(trn => $msg->trn, ot => $msg->ot, o_r => 'R', ack => 1);
-                $wbuf .= sprintf "\x02%s\x03", $rpl->as_string;
-            };
-        }
-        $eof = 1 unless $len;
+my $server = IO::Socket::INET->new(%opts) or die "$0: server: $!";
+
+while (my $fh = $opts{Listen} ? $server->accept : $server) {
+    my $rbuf = my $wbuf = my $eof = '';
+    my $sel = IO::Select->new($fh);
+    while (not $eof) {
+        while (length $wbuf and my @ready = $sel->can_write(0)) {
+            my $len = syswrite $fh, $wbuf, 1024;
+            $wbuf = substr $wbuf, $len if $len;
+        };
+        if (my @ready = $sel->can_read(0)) {
+            my $len = sysread $fh, my $tmp, 1024;
+            $rbuf .= $tmp if $len;
+            while ($rbuf =~ s/.*?\x02(.*?)\x03//s) {
+                my $str = $1;
+                my $msg = eval { Protocol::EMIUCP::Message->new_from_string($str) };
+                my $rpl = do {
+                    if (my $e = $@) {
+                        # Cannot parse EMI-UCP message
+                        warn $e;
+                        Protocol::EMIUCP::Message->new(trn => $e->trn, ot => $e->ot, o_r => 'R', nack => 1, ec => EC_SYNTAX_ERROR)
+                            if (($e->o_r||'') eq 'O');
+                    }
+                    elsif ($msg and $msg->o_r eq 'O') {
+                        # Reply only for Operation
+                        if ($msg->ot =~ /^(01|51|60)$/) {
+                            # OT allowed by SMSC
+                            my %sm = do {
+                                if ($msg->ot =~ /^(01|51)$/) {
+                                    my @t = localtime;
+                                    $t[5] %= 100;
+                                    +(sm => $msg->oadc . ':' . sprintf '%02d%02d%02d%02d%02d%02d', @t[3,4,5,2,1,0]);
+                                }
+                                else {
+                                    +();
+                                };
+                            };
+                            Protocol::EMIUCP::Message->new(trn => $msg->trn, ot => $msg->ot, o_r => 'R', ack => 1, %sm);
+                        }
+                        else {
+                            # Not allowed by SMSC
+                            Protocol::EMIUCP::Message->new(trn => $msg->trn, ot => $msg->ot, o_r => 'R', nack => 1, ec => EC_OPERATION_NOT_ALLOWED);
+                        };
+                    }
+                };
+                $wbuf .= sprintf "\x02%s\x03", $rpl->as_string if $rpl;
+            }
+            $eof = 1 unless $len;
+        };
     };
 };
