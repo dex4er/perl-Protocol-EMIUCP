0d36ae52443cb70af75d53e6efb608e6f00dbe2a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-09-18 01:20:22 +0200

    Be more verbose

--- examples/fakesmsc-iosocket.pl
+++ examples/fakesmsc-iosocket.pl
@@ -55,6 +55,7 @@ while (my $fh = $opts{Listen} ? $server->accept : $server) {
                             if $e->o;
                     }
                     elsif ($msg and $msg->o) {
+                        printf "<<< [%s]\n", $msg->as_string;
                         # Reply only for Operation
                         if ($msg->ot =~ /^(01|51|60)$/) {
                             # OT allowed by SMSC
@@ -76,7 +77,10 @@ while (my $fh = $opts{Listen} ? $server->accept : $server) {
                         };
                     };
                 };
-                $wbuf .= sprintf "\x02%s\x03", $rpl->as_string if $rpl;
+                if ($rpl) {
+                    printf ">>> [%s]\n", $rpl->as_string;
+                    $wbuf .= sprintf "\x02%s\x03", $rpl->as_string;
+                };
             };
             $eof = 1 unless $len;
         };
