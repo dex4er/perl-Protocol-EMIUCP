eb991756a879eef54d5025c57111c473ea9dbf83
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-11-02 11:20:40 +0100

    Use POSIX::strftime::GNU also on interix and cygwin

--- script/emiclient.pl
+++ script/emiclient.pl
@@ -8,7 +8,7 @@
 use strict;
 use warnings;
 
-use if $^O eq 'MSWin32' => 'POSIX::strftime::GNU';
+use if $^O =~ /^(MSWin32|cygwin|interix)$/ => 'POSIX::strftime::GNU';
 
 use Protocol::EMIUCP::Connection;
 use Protocol::EMIUCP::Message;
